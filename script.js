28e1ebd61a5d9f072de4d22a1821f1ed29031c24
magnusFT <58776797+magnusFT@users.noreply.github.com>
2023-10-26 11:00:23 +0200

    Completion % bar
    stylet litt, displayer prosent av antall checka bokser
    
    #5

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+let totalCheckboxes = 0; //antall checkbokser
+let completedCheckboxes = 0; // aantall markerte checkboxer
+
+
 function checkEnter(event) {
     if (event.key === "Enter") {
         event.preventDefault(); 
         addtext(); 
+        updateCompletionPercentage();
     }
 }
 
@@ -10,7 +15,7 @@ function addtext() {
     let brukerSvar = document.getElementById("input_felt").value;
 
     if (brukerSvar.trim() === "") {
-      alert("Please enter some text before adding it.");
+      alert("Vennligst skriv tekst før du submiter.");
       return;
     }
 
@@ -19,6 +24,7 @@ function addtext() {
     // lage checkbox element
     let checkbox = document.createElement("input");
     checkbox.type = "checkbox";
+    checkbox.addEventListener("change", updateCompletionPercentage);
 
      // lag en label element for chekcbokssssss
     let label = document.createElement("label");
@@ -33,7 +39,16 @@ function addtext() {
     test.appendChild(textNode);
     test.appendChild(document.createElement("br"));
 
+    // Øk antall checkbokser
+    totalCheckboxes++;
+
     // fjern tidligere tekst i input felt
     document.getElementById("input_felt").value = "";
   }
 
+  function updateCompletionPercentage() { // Oppdater completion %
+    completedCheckboxes = document.querySelectorAll("input[type='checkbox']:checked").length;
+    let completionPercentage = (completedCheckboxes / totalCheckboxes) * 100;
+    let completionPercentageDisplay = document.getElementById("completionPercentage");
+    completionPercentageDisplay.textContent = `Completion: ${completionPercentage.toFixed(2)}%`;
+}
